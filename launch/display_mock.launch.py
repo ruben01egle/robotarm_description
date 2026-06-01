2ac49fa7a4f71a2fcfa49b592e63f2f8bdab5f5a
Ruben Egle <ruben01egle@gmail.com>
2026-06-01 16:33:30 +0200

    added launch files

--- launch/display_mock.launch.py
+++ launch/display_mock.launch.py
@@ -2,7 +2,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import Command, FindExecutable
+from launch.substitutions import Command
 
 def generate_launch_description():
     # Holt den Installationspfad dieses Pakets
@@ -10,6 +10,18 @@ def generate_launch_description():
     xacro_file = os.path.join(pkg_share, 'urdf', 'robotarm.urdf.xacro')
     rviz_config_path = os.path.join(pkg_share, 'rviz', 'urdf_config.rviz')
 
+    # Startet den robot_state_publisher und füttert ihn mit dem geparsten Xacro
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', xacro_file])}]
+    )
+
+    # Erzeugt Schieberegler in einem kleinen Extra-Fenster, um die Gelenke in RViz zu drehen
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui'
+    )
 
     # Startet RViz2
     rviz_node = Node(
@@ -21,5 +33,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        robot_state_publisher_node,
+        joint_state_publisher_node,
         rviz_node
     ])
